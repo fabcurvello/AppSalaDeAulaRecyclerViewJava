145bdaa31fe256002f252ec2a68d5477f2625578
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-10-03 13:19:39 -0300

    Etapa 2 concluida. Já é possível carregar no RecyclerView uma Lista de dados.

--- app/src/main/java/br/com/fabriciocurvello/appsaladeaularecyclerviewjava/activity/MainActivity.java
+++ app/src/main/java/br/com/fabriciocurvello/appsaladeaularecyclerviewjava/activity/MainActivity.java
@@ -108,6 +108,33 @@ public class MainActivity extends AppCompatActivity {
 
         d = new Disciplina("Desevolvimento de Software para Web","Juan Baptista","SEX","LAB 02");
         disciplinas.add(d);
+
+        d = new Disciplina("Estatística Computacional","Wellington Avila","SEG","SALA 107" );
+        disciplinas.add(d);
+
+        d = new Disciplina("Teoria de Sistemas da Informação","Wellington Avila","TER","SALA 109" );
+        disciplinas.add(d);
+
+        d = new Disciplina("Banco de Dados I","Fabrício Curvello","QUA","LAB 05");
+        disciplinas.add(d);
+
+        d = new Disciplina("Arquitetura de Computadores","Wellington Avila","QUA","LAB 05");
+        disciplinas.add(d);
+
+        d = new Disciplina("Programação Orientada a Objetos","Fabrício Curvello","QUA","LAB 04");
+        disciplinas.add(d);
+
+        d = new Disciplina("Computação para Dispositivos Móveis","Fabrício Curvello","QUI","LAB 02");
+        disciplinas.add(d);
+
+        d = new Disciplina("Estrutura de Dados","Juan Baptista","QUI","LAB 04");
+        disciplinas.add(d);
+
+        d = new Disciplina("Interface Humano-Computador","Wellington Avila","SEX","SALA 109");
+        disciplinas.add(d);
+
+        d = new Disciplina("Desevolvimento de Software para Web","Juan Baptista","SEX","LAB 02");
+        disciplinas.add(d);
     }
 
 }
